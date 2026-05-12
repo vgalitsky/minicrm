ebf383c8590a3c5b82bd009dde3add5c747434cc
vgalitsky <concept.galitsky@gmail.com>
2026-05-12 11:53:33 -0400

    structured service provider

--- packages/mtr/mini-crm/src/MiniCrmServiceProvider.php
+++ packages/mtr/mini-crm/src/MiniCrmServiceProvider.php
@@ -13,12 +13,63 @@ class MiniCrmServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this
+            ->loadMigrations()
+            ->loadRoutes()
+            ->loadViews()
+            ->registerCommands();
+    }
+
+    /**
+     * Load package migrations.
+     * 
+     * @return $this
+     */
+    protected function loadMigrations(): static
+    {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
+        return $this;
+    }
+
+    /**
+     * Load package routes.
+     * 
+     * @return $this
+     */
+    protected function loadRoutes(): static
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        return $this;
+    }
+
+    /**
+     * Load package views.
+     * 
+     * @return $this
+     */
+    protected function loadViews(): static
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'mini-crm');
+
+        return $this;
+    }
+
+    /**
+     * Register package commands.
+     * 
+     * @return $this
+     */
+    protected function registerCommands(): static
+    {
         if ($this->app->runningInConsole()) {
             $this->commands([
                 InstallCommand::class,
             ]);
         }
+        
+        return $this;
     }
 }
