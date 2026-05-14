bbc2d8c0c46436661b411630edf23449213d78e3
vgalitsky <concept.galitsky@gmail.com>
2026-05-14 06:02:30 -0400

    Console Commands: modify install command: refactor + added publishing

--- packages/mtr/mini-crm/src/Console/Commands/InstallCommand.php
+++ packages/mtr/mini-crm/src/Console/Commands/InstallCommand.php
@@ -4,30 +4,47 @@ namespace Mtr\MiniCrm\Console\Commands;
 
 use Illuminate\Console\Command;
 use Mtr\MiniCrm\Database\Seeders\MiniCrmSeeder;
+use Mtr\MiniCrm\MiniCrmServiceProvider;
 
 class InstallCommand extends Command
 {
-    protected $signature = 'minicrm:install {--fresh : Drop & migrate} {--seed : Seed data}';
+    protected $signature = 'minicrm:install
+        {--seed : Seed data}
+        {--publish : Publish config, views, and assets}';
 
     protected $description = 'Install Mini CRM package';
 
     /**
-     * @return void
+     * @return int
      */
-    public function handle(): void
+    public function handle(): int
     {
         $this->info('Installing Mini CRM...');
 
-        if ($this->option('fresh')) {
-            $this->call('migrate:fresh');
-        } else {
-            $this->call('migrate');
+        if ($this->option('publish')) {
+            $this->call('vendor:publish',
+                [
+                    '--provider' => MiniCrmServiceProvider::class, 
+                    '--tag' => 'minicrm-config'
+                ]
+            );
+
+            $this->call('vendor:publish',
+                [
+                    '--provider' => MiniCrmServiceProvider::class, 
+                    '--tag' => 'minicrm-assets'
+                ]
+            );
         }
+
+        $this->call('migrate');
 
         if ($this->option('seed')) {
             $this->call('db:seed', ['--class' => MiniCrmSeeder::class]);
         }
 
         $this->info('Mini CRM installed successfully!');
+
+        return self::SUCCESS;
     }
 }
